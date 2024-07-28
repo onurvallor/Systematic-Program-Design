f195c0ac51570bd970cee2baeb48180743c4e505
Onur Vallor <onurvallor@gmail.com>
2024-07-27 23:05:29 -0400

    fixed template errors and implemented countdown-to-image function

--- 2. How To Design Data/countdown.rkt
+++ 2. How To Design Data/countdown.rkt
@@ -1,6 +1,8 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname countdown) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+
 ;; CountDown is one of:
 ;; - false
 ;; - Natural [1, 10]
@@ -15,10 +17,11 @@
 (define CD3 1)
 (define CD4 "complete")
 
+#;
 (define (fn-for-countdown c)
-  (cond [false? (...)]
+  (cond [(false? c) (...)]
         [(and (number? c) (<= c 10) (>= c 1)) (... c)]
-        [(string=? "complete") (...)]
+        [(string=? "complete" c) (...)]
   ))
 
 ;; Template rule used:
@@ -26,3 +29,24 @@
 ;; - atomic distinct:     false
 ;; - atomic non-distinct: Natural[1, 10]
 ;; - atomic distinct:     "complete"
+
+;; FUNCTIONS:
+
+;; CountDown -> Image
+;; produce an image of current state of countdown.
+(check-expect (countdown-to-image false)  (square 0 "solid" "white"))
+(check-expect (countdown-to-image 3) (text (number->string 3) 24 "black"))
+(check-expect (countdown-to-image "complete") (text "HAPPY NEW YEAR!" 24 "red"))
+
+;(define (countdown-to-image c) (square 0 "solid" "white")) ;stub
+
+;<used template from Countdown>
+
+(define (countdown-to-image c)
+  (cond [(false? c)(square 0 "solid" "white")]
+        [(and (number? c) (<= c 10) (>= c 1)) (text (number->string c) 24 "black")]
+        [(string=? "complete" c) (text "HAPPY NEW YEAR!" 24 "red")]
+  ))
+
+
+
