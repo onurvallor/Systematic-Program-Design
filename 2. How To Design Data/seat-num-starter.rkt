b701527060b333cfda43eb6707c0a30042af314c
Onur Vallor <onurvallor@gmail.com>
2024-07-27 20:09:39 -0400

    seat-num-starter added.

---
+++ 2. How To Design Data/seat-num-starter.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname seat-num-starter) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; numSeat is Natural[1, 32]
+;; interp. a seat number between 1 and 32 where 1 and 32 are aisle seats.
+(define SN1  1)  ;aisle
+(define SN2 12) ;middle
+(define SN3 32) ;aisle
+
+#;
+(define (fn-for-seat-num sn)
+  (... sn))
+
+;; Template rules used:
+;; - atomic non-distinct: Natural[1, 32]
